cff7c189119c808fa19a70f92eae3a81ee1be90f
christian_hpc <ticachristian20@gmail.com>
2025-10-08 03:25:25 +0800

    Updated the contents of the README.md to reflect the cahnges

--- Electric/2h_mom_3kdig/results/mpfr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm result
-
-mpicxx -I/usr/local/include/boost/ -o result result.cpp -L/usr/local/lib -lboost_mpi -lboost_serialization -lboost_thread -lboost_system -lboost_chrono -pthread -lmpfr -lgmp
-
-mpirun --oversubscribe -np 1 ./result
